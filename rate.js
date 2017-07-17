568ced90135d32b649f31270155b5ac960cfcb52
osk2 <sc60714@gmail.com>
2017-07-17 11:29:18 +0800

    Update rate.js

--- rate.js
+++ rate.js
@@ -25,9 +25,31 @@ module.exports = [
     'rate': 0
   },
   {
+    'item': '活動回饋',
+    'match': /^活動類.*$/,
+    'rate': 0,
+    'note': '特殊活動不計算回饋'
+  },
+  {
+    'item': '高鐵',
+    'match': /^高鐵ＥＣ.*$/,
+    'rate': 0.02
+  },
+  {
+    'item': '台鐵',
+    'match': /^ｒａｉｌｗａｙTAIPEI\/TW$/,
+    'rate': 0.02
+  },
+  {
     'item': '支付寶（淘寶／天貓）',
     'match': /^ALIPAY\*TAOBAO\.COM.*$/,
     'rate': 0.02
+  },
+  {
+    'item': '支付寶（淘寶／天貓）',
+    'match': /www\.taobao\.com/,
+    'rate': 0.02,
+    'contributor': 'AIC'
   },
   {
     'item': '蝦皮拍賣',
@@ -38,13 +60,13 @@ module.exports = [
     'item': '街口支付',
     'match': /^街口﹣.*$/,
     'rate': 0.02,
-    'note': '街口支付有"街口-"和"街口支付-"兩種前綴'
+    'note': '街口支付有`街口-`和`街口支付-`兩種前綴'
   },
   {
     'item': '街口支付',
     'match': /^街口支付﹣.*$/,
     'rate': 0.02,
-    'note': '街口支付有"街口-"和"街口支付-"兩種前綴'
+    'note': '街口支付有`街口-`和`街口支付-`兩種前綴'
   },
   {
     'item': 'Lativ',
@@ -52,10 +74,15 @@ module.exports = [
     'rate': 0.02
   },
   {
+    'item': 'UNIQLO 網購',
+    'match': /^台灣優衣庫有限公司ＥＣ.*$/,
+    'rate': 0.02
+  },
+  {
     'item': 'PChome',
     'match': /^網路家庭國際資訊股份.*$/,
     'rate': 0.02,
-    'note': '若為分期字串會被截斷，故只能判斷至"股份"'
+    'note': '若為分期字串會被截斷，故只能判斷至`股份`'
   },
   {
     'item': 'Airbnb',
@@ -74,7 +101,7 @@ module.exports = [
   },
   {
     'item': 'Google Wallet',
-    'match': /^GOOGLE \*.*$/,
+    'match': /^GOOGLE ?\*.*$/,
     'rate': 0.02
   },
   {
@@ -88,7 +115,7 @@ module.exports = [
     'rate': 0.02
   },
   {
-    'item': '小米',
+    'item': '小米網購',
     'match': /^台灣小米通訊有限公司.*$/,
     'rate': 0.02
   },
@@ -103,8 +130,30 @@ module.exports = [
     'rate': 0.02
   },
   {
+    'item': 'STEAM',
+    'match': /WWW\.STEAMPOWERED\.COM.*$/,
+    'rate': 0.02
+  },
+  {
     'item': 'Yahoo',
     'match': /^雅虎奇摩.*$/,
     'rate': 0.02 
+  },
+  {
+    'item': 'Yahoo 輕鬆付',
+    'match': /^雅虎輕鬆付.*$/,
+    'rate': 0.02 
+  },
+  {
+    'item': '迪卡儂網購',
+    'match': /^ＤＥＣＡＴＨＬＯＮ.*$/,
+    'rate': 0.02,
+    'note': '若於實體店面使用 Apple Pay 等支付有機會列入數位通路'
+  },
+  {
+    'item': 'iherb',
+    'match': /^iHerb.*$/,
+    'rate': 0.02,
+    'contributor': 'AIC'
   }
 ]
