fb29c88104fd51b42c6847ffb075f6ffc667de01
osk2 <sc60714@gmail.com>
2018-08-01 15:47:38 +0800

    Complete list page

---
+++ src/components/list-item.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="row item">
+    <div class="col-md-10 col-xs-8 item-detail">
+      <h4>
+        {{item.item}}
+        <br>
+        <small class="text-muted" v-if="item.note">{{item.note}}</small>
+        <small v-if="item.contributor" class="text-muted">
+          <i class="fa fa-user" aria-hidden="true"></i> 
+          本通路由 {{item.contributor}} 提供
+        </small>
+      </h4>
+    </div>
+    <div
+      :class="[
+        'col-md-2',
+        'col-xs-4',
+        'item-type',
+        getRateDescription(item.rate).rateClass]"
+    >
+      <h4 class="white text">
+        <span style="font-size: 1.5em">{{getRateDescription(item.rate).rateNumber}}</span>
+        <br>
+        <small class="white text">{{getRateDescription(item.rate).rateText}}</small>
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    item: Object,
+  },
+  methods: {
+    getRateDescription(rate) {
+      const isBonus = rate > 0;
+      let rateClass = '';
+      let rateText = '';
+      let rateNumber = '';
+
+      rateClass = isBonus ? 'bonus' : 'no-rebate';
+      rateText = isBonus ? '指定通路加碼' : '無現金回饋';
+      rateNumber = (rate * 100).toString() + '%';
+
+      return {rateClass, rateText, rateNumber};
+    }
+  }
+};
+</script>
+
+<style lang="scss" scope>
+  .text.white {
+    color: #ffffff;
+  }
+
+  .item {
+    margin-bottom: 15px;
+    background: #f8f8f8;
+    border-radius: 4px;
+    font-size: 1.1em;
+  }
+
+  .item-type {
+    padding: 15px;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+
+    &.bonus {
+      background: #59c3c3;
+    }
+
+    &.no-rebate {
+    background: #f45b69;
+    }
+  }
+
+  .item-detail {
+    padding: 25px 15px 15px 30px;
+  }
+
+</style>
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div
+      v-if="items.length > 0"
+      v-for="(item, key) in items"
+      :key="key"
+    >
+      <list-item :item="item"></list-item>
+    </div>
   </div>
 </template>
+
+<script>
+import ListItem from '../components/list-item.vue';
+import items from '../../lib/rate.js';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+export default {
+  name: 'list',
+  data() {
+    return {
+      items
+    }
+  },
+  components: { ListItem }
+}
+</script>
+
